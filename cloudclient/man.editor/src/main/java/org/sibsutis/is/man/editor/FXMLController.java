03043e32492402ec2d0b8721b1605e47eb3dc6be
vaganovdv <vaganovdv@pc>
2017-04-01 19:27:24 +1000

    Добавлено окно регистрации

--- cloudclient/man.editor/src/main/java/org/sibsutis/is/man/editor/FXMLController.java
+++ cloudclient/man.editor/src/main/java/org/sibsutis/is/man/editor/FXMLController.java
@@ -2,8 +2,15 @@ package org.sibsutis.is.man.editor;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 
 
 public class FXMLController implements Initializable
@@ -12,11 +19,14 @@ public class FXMLController implements Initializable
 // лог
     private static final Logger log = Logger.getLogger(FXMLController.class.getName());
 
+// Форма регистрации студента 
 
+@FXML
+private TextField Suname_TF;
+@FXML
+private Button Add_Person_BT;
   
     
-
-
 
 
     @Override
@@ -25,9 +35,25 @@ public class FXMLController implements Initializable
         log.info("----------------------------------------------------------------------");
         log.info(" ...");
         //ConfigureModules();
-        //ConfigureButtons();
+        ConfigureButtons();
         log.info("Конфигурация зависимых модулей завершена");
         log.info("----------------------------------------------------------------------");
+    }
+
+    private void ConfigureButtons()
+    {
+        Configure_Add_Person_BT();
+    }
+
+    private void Configure_Add_Person_BT()
+    {
+        Add_Person_BT.setOnAction((ActionEvent event) ->
+        {// Обработка нажатия клавиши
+
+            log.log(Level.INFO, "[man.editor] Нажата кнопка добавления студента {Добавить}");  
+
+        } // Обработка нажатия клавиши
+        );
     }
 
 }

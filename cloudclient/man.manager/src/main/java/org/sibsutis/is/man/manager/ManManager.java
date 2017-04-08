2880ad012398f074fe49c40c4c99e7f5ecc6083a
vaganovdv <vaganovdv@pc>
2017-04-08 19:04:34 +1000

    Добавлен менеждер [08.04.2017 19:04]

--- cloudclient/man.manager/src/main/java/org/sibsutis/is/man/manager/ManManager.java
+++ cloudclient/man.manager/src/main/java/org/sibsutis/is/man/manager/ManManager.java
@@ -7,6 +7,8 @@ package org.sibsutis.is.man.manager;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.sibsutis.is.man.model.Man;
 import org.sibsutis.is.man.model.ManManagerAPI;
 
@@ -17,14 +19,34 @@ import org.sibsutis.is.man.model.ManManagerAPI;
 
 public class ManManager implements ManManagerAPI
 {
+
+    private static final Logger log = Logger.getLogger(ManManager.class.getName());
+    
+    
     private final ConcurrentMap <Long,  Man > ManDatabase  = new ConcurrentHashMap < >();
 
     @Override
     public boolean addMan(Man man)
     {
        boolean result = false;
+       if (man !=null)
+       {
        
-       
+           if (ManDatabase.containsKey(man.getId()))
+           {
+            log.log(Level.WARNING, "[ManManager] Невозможно  экземпляр [Man] класса - id = ["+man.getId()+"] существует в БД");
+           } 
+           else
+           {
+                ManDatabase.put(man.getId(), man);
+                 log.log(Level.INFO, "[ManManager]  экземпляр [Man]  id = ["+man.getId()+"]  успешно добавлен  в БД");
+           }
+           
+       }  
+       else
+       {
+       log.log(Level.WARNING, "[ManManager] Невозможно добавить пустой экземпляр [Man]");
+       }
        
        return result;
     }
--- cloudclient/man.model/src/main/java/org/sibsutis/is/man/model/Man.java
+++ cloudclient/man.model/src/main/java/org/sibsutis/is/man/model/Man.java
@@ -10,48 +10,16 @@ import org.sibsutis.is.location.AbstractLocation;
 public class Man
 {
 
-    /**
-     * @return the id
-     */
-    public long getId()
-    {
-        return id;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(long id)
-    {
-        this.id = id;
-    }
-
     private static final Logger log = Logger.getLogger(Man.class.getName());
-
-    public String getMiddleName()
-    {
-        log.log(Level.INFO, "   ");        
-        return MiddleName;
-    }
-
     
 
-    
-    
-    
-    
-    
-    public void setMiddleName(String MiddleName)
-    {
-        this.MiddleName = MiddleName;
-    }
-    
     private long id;                                        // Идентификатор человека в БД
     private String FistName;                        // Имя человека 
     private String SureName;                        // Фамилия человека     
     private String MiddleName;                     // Отчество  человека 
     private LocalDateTime   BirthDate;          //Дата рождения
     private AbstractLocation BirthLocation;
+    private static long count=0L;
     
     
     
@@ -60,9 +28,11 @@ public class Man
     public Man (  )
     {        
         log.log(Level.INFO, "[Man]   Создание экземпляра класса..."); 
-        this.FistName = "";
-        this.MiddleName = "";
-        this.SureName = "";
+        this.FistName       =  "";
+        this.MiddleName  = "";
+        this.SureName      =  "";
+        this.count++;
+        this.id = count;
         log.log(Level.INFO, "[Man]   Создание экземпляра класса закончено");   
     }       
     
@@ -89,11 +59,32 @@ public class Man
         this.SureName = SureName;
     }
 
-    
-    
     public String getFullName ()
     {
         return this.getFistName()+" "+this.getMiddleName()+" "+this.getSureName();    
     }    
     
+        public long getId()
+    {
+        return id;
+    }
+
+   
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+    
+      
+    public void setMiddleName(String MiddleName)
+    {
+        this.MiddleName = MiddleName;
+    }
+    
+    
+    public String getMiddleName()
+    {
+        log.log(Level.INFO, "   ");        
+        return MiddleName;
+    }
 }

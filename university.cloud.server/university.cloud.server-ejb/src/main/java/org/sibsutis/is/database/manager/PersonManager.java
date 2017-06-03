95d760a8bca6b6e54c3deb9b65c994cd25bd106b
vaganovdv <vaganovdv@pc>
2017-06-03 18:55:59 +1000

    Добавил тест базы

--- university.cloud.server/university.cloud.server-ejb/src/main/java/org/sibsutis/is/database/manager/PersonManager.java
+++ university.cloud.server/university.cloud.server-ejb/src/main/java/org/sibsutis/is/database/manager/PersonManager.java
@@ -5,10 +5,16 @@
  */
 package org.sibsutis.is.database.manager;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
+import org.sibsutis.is.database.facade.EmployeeFacade;
 import org.sibsutis.is.database.facade.StudentFacade;
+import org.sibsutis.is.database.facade.StudentGroupFacade;
+import org.sibsutis.is.database.model.entity.Employee;
 import org.sibsutis.is.database.model.entity.Student;
+import org.sibsutis.is.database.model.entity.StudentGroup;
 
 /**
  *
@@ -19,15 +25,47 @@ public class PersonManager
 {
 
     @EJB
+    private StudentGroupFacade studentGroupFacade;
+
+    @EJB
+    private EmployeeFacade employeeFacade;
+
+    @EJB
     private StudentFacade studentFacade;
+    
+    
+    
+    
 
     public void start()
     {
-        Student student  =new Student();
-        student.setName("Владимир");
-        student.setSureName("Путин");
-        studentFacade.create(student);
+        Student student1  =new Student();
+        student1.setName("Владимир");
+        student1.setMiddleName("Владимирович");
+        student1.setSureName("Путин");
+        studentFacade.create(student1);
         
+        Student student2  =new Student();
+        student2.setName("Владимир");
+        student1.setMiddleName("Вольфович");
+        student2.setSureName("Жириновский");
+        studentFacade.create(student2);
+        
+        Employee employee =new    Employee ();
+        employee.setName("Владимир");
+        employee.setSureName("Жириновский");
+        employee.setMiddleName("Вольфович");
+        employee.setPosition("Директор ХИИК");
+        employeeFacade.create(employee);
+        
+        StudentGroup  studentGroup = new StudentGroup();
+        List <Student> StudentDatabase =new ArrayList<>();
+        StudentDatabase.add(student1);
+        StudentDatabase.add(student2);
+        
+        studentGroup.setStudents(StudentDatabase);
+       
+        studentGroupFacade.create(studentGroup);
         
     }
     

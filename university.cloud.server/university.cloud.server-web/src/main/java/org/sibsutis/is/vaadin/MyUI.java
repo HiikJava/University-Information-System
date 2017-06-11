450875983e22deeb797fcd3b8a0abab8781b61a4
vaganovdv <vaganovdv@pc>
2017-06-11 22:11:24 +1000

    Добавили поддержку Vaadin из модуляграфики

--- university.cloud.server/university.cloud.server-web/src/main/java/org/sibsutis/is/vaadin/MyUI.java
+++ university.cloud.server/university.cloud.server-web/src/main/java/org/sibsutis/is/vaadin/MyUI.java
@@ -4,6 +4,7 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.cdi.CDIUI;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
@@ -11,6 +12,11 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import org.sibsutis.is.database.facade.StudentFacade;
+import org.sibsutis.is.database.model.entity.Student;
 
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
@@ -19,29 +25,71 @@ import com.vaadin.ui.VerticalLayout;
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
  * overridden to add component to the user interface and initialize non-component functionality.
  */
+@CDIUI
 @Theme("mytheme")
-public class MyUI extends UI {
+public class MyUI extends UI 
+{
 
+   
+private static final Logger log = Logger.getLogger(MyUI.class.getName());
+
+  @EJB
+    private StudentFacade studentFacade;
+  
+
+  
+  
     @Override
-    protected void init(VaadinRequest vaadinRequest) {
+    protected void init(VaadinRequest vaadinRequest)
+    {
+        
+        
         final VerticalLayout layout = new VerticalLayout();
         
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
-
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
+        final TextField fam = new TextField();
+        final TextField i = new TextField();
+        final TextField o = new TextField();
+        
+        fam.setCaption("Введите фамилию:");
+        i.setCaption("Введите Имя:");
+        o.setCaption("Введите отчество:");
+        
+        
+        
+        Button button = new Button("Сохранить");
+        button.addClickListener( e -> 
+        {
+            layout.addComponent(new Label("Thanks " + fam.getValue() 
                     + ", it works!"));
+            
+            log.log(Level.INFO,"Попытка сохранения в базe данных... ");
+            if (studentFacade !=null)
+            {
+                log.log(Level.INFO,"Менеджер {Person } доступен");
+                Student  s = new Student();
+                s.setName("Иван");
+                s.setSureName("Иванов");
+                studentFacade.create(s);
+                      log.log(Level.INFO,"Менеджер {Person } экземпляр класса сохранен в БД"); 
+                
+            } 
+            else
+                {
+            log.log(Level.WARNING,"Менеджер {Person } не  доступен");
+        }
+            
         });
         
-        layout.addComponents(name, button);
+        layout.addComponents(fam,i,o, button);
         
         setContent(layout);
     }
+    
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
+
+    
 }

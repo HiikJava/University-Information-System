c44d68170ee531cbc1fe6a60dbff1a2a9d0f1608
mitrony <mitrony@putin>
2017-06-24 01:02:30 +1000

    Отладка PrimeFaces

--- university.cloud.server/university.cloud.server-web/src/main/java/org/sibsutis/is/university/cloud/server/web/terminalView.java
+++ university.cloud.server/university.cloud.server-web/src/main/java/org/sibsutis/is/university/cloud/server/web/terminalView.java
@@ -1,9 +1,12 @@
 
 package org.sibsutis.is.university.cloud.server.web;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.faces.bean.ManagedBean;
 import org.sibsutis.is.database.facade.StudentFacade;
+import javax.faces.bean.ManagedBean;
+import org.sibsutis.is.cloud.ignite.IgniteManager;
 /**
  *
  * @author developer
@@ -14,9 +17,16 @@ import org.sibsutis.is.database.facade.StudentFacade;
 @ManagedBean
 public class terminalView 
 {
-
+    private static final Logger log = Logger.getLogger(IgniteManager.class.getName());
+    
+    private String firstname;
+    private String lastname;
+    private String middlename;
+    private String fullname;
+    
     @EJB
     private StudentFacade studentFacade;
+        
 
     public String handleCommand(String command, String[] params) {
         if(command.equals("greet")) {
@@ -25,6 +35,32 @@ public class terminalView
         else {
             return "it's bad";
         }
-        
-}
+    }
+    
+    public String getfirstname(){
+        return firstname;
+    }
+    public void setfirstname(String firstname){
+        this.firstname = firstname;
+    }
+    
+    public String getlastname(){
+        return lastname;
+    }
+    public void setlastname(String lastname){
+        this.lastname = lastname;
+    }
+    
+    public String getmiddlename(){
+        return middlename;
+    }
+    public void setmiddlename(String middlename){
+        this.middlename = middlename;
+    }
+    
+    public String getfullname(){
+        fullname = lastname+" "+firstname+" "+middlename;
+        log.log(Level.INFO, "[terminalView] полное имя: "+fullname);
+        return fullname;
+    }
 }
